363bfec508535f7dda0e061b70b72cbf2fdfcb58
quan <tran.q90@gmail.com>
2019-03-25 13:41:44 -0700

    asd

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,23 @@
 $("button").click(function (e) {
     var search = $("#searchTerm").val();
     e.preventDefault();
-    console.log(search);
     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "&api-key=9pElGwiXwurYGOMRNxb2KcjKFfcOEjm1"
-    console.log(queryURL);
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         console.log(response);
-        var title = response.response.docs[0].headline.main;
-        $("#topArticles").text(title);
-        console.log(title);
+        var title = response.response.docs[i].headline.main;
+        var author = response.response.docs[i].byline.original;
 
         
-        
+        for (var i = 0; i < $("#numberRecords").val(); i++) {
+            $("#topArticles").text(response.response.docs[i].headline.main);  
+        }
 
-        
+
+
 
 
     })
