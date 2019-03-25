df11f62b7ebc2e83796a70d59879e1ee5a189bd7
quan <tran.q90@gmail.com>
2019-03-25 13:22:05 -0700

    can search one

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,24 @@
-$.ajax({
+$("button").click(function (e) {
+    var search = $("#searchTerm").val();
+    e.preventDefault();
+    console.log(search);
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "&api-key=9pElGwiXwurYGOMRNxb2KcjKFfcOEjm1"
+    console.log(queryURL);
 
-}).then(function(response){
-    var results = response.data;
-    console.log(results);
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        var title = response.response.docs[0].headline.main;
+        $("#topArticles").text(title);
+        console.log(title);
 
-    for (var i = 0; i < results.length; i++){
-        var resultDiv = $("<div>");
-        var author = results[i];
-    }
+        
+        
+
+        
+
+
+    })
 })
